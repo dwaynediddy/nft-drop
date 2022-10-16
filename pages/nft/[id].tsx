1e8abb3a07ef700f08e853cdadd2d7fb97ff708f
dwaynediddy <burrows.dwayne@icloud.com>
2022-10-16 19:17:43 +0200

    added thirdweb for wallet connection

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useAddress, useMetamask, useDisconnect } from "@thirdweb-dev/react";
+
 
 const NFTDropPage = () => {
+    // Auth -- 
+    const connectWithMetamask = useMetamask()
+    const address = useAddress()
+    const disconnect = useDisconnect()
   return (
     <div className='flex h-screen flex-col lg:grid lg:grid-cols-10'>
         <div className='bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4'>
@@ -28,10 +34,17 @@ const NFTDropPage = () => {
                     NTF Marketplace
                 </h1>
 
-                <button className='rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base'>Sign In</button>
+                <button 
+                    onClick={() => address ? disconnect() : connectWithMetamask()}
+                    className='rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base'>
+                        {address ? 'Sign Out' : 'Sign In'}
+                </button>
             </header>
 
             <hr className='my-2 border' />
+            {address && (
+                <p className='text-center text-sm text-rose-400'>You are logged in with wallet {address.substring(0, 5)}...{address.substring(address.length - 5)}</p>
+            )}
 
             <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center'>
                 <img className='w-80 object-cover pb-10 lg:h-40' src='https://tse4.mm.bing.net/th?id=OIP.9pH7663T6YAA31aSyA1cugHaDt&pid=Api&P=0' alt='' />
@@ -39,7 +52,7 @@ const NFTDropPage = () => {
                 <p className='pt-2 text-xl text-green-500'>11/24 nfts claimed</p>
             </div>
             <button className='h-16 bg-red-500 text-white rounded-full mt-10 font-bold'>
-                Mint NFT (0.02btc)
+                Mint NFT (0.02 eth)
             </button>
         </div>
     </div>
